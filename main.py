71fb939c691650d8c79f7b4b674fdaa23acebf97
RingLord54 <andrew.moneypenny@btinternet.com>
2022-05-19 11:59:57 +0100

    Main Application window added
    - Title Added
    - Dimensions set
    - Window set so it cannot be resized

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk() # Creates main Application window
+
+root.geometry("1440x720") # Sets the dimensions of the Application window to 1440 x 720
+root.title("Hangman") # Sets the Title of the Application window to "Hangman"
+root.resizable(False, False) # Sets it so that the Application window can't be resized
+
+root.mainloop() # Runs the Application window
+
