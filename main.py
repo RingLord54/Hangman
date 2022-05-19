9fb1f524eebe1273d94ab35e9f87192b3b3bc3d7
RingLord54 <andrew.moneypenny@btinternet.com>
2022-05-19 12:19:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ root.geometry("1000x800") # Sets the dimensions of the Application window to 144
 root.title("Hangman") # Sets the Title of the Application window to "Hangman"
 root.resizable(False, False) # Sets it so that the Application window can't be resized
 
-Frame_top = Frame( root, bg='red', width=960, height=60)
-Frame_Lmiddle1 = Frame( root, bg='blue', width=450, height=500 )
-Frame_Lmiddle2 = Frame( root, bg='green', width=450, height=160 )
-Frame_Rmiddle = Frame( root, bg='yellow', width=450, height=350)
+Frame_top = Frame( root, bg='red', width=960, height=60) # Creates Frame for Title
+Frame_Lmiddle1 = Frame( root, bg='blue', width=450, height=500 ) # Creates Frame for the Picture
+Frame_Lmiddle2 = Frame( root, bg='green', width=450, height=160 ) # Creates Frame for where the word will be guessed
+Frame_Rmiddle = Frame( root, bg='yellow', width=450, height=350) # Creates Frame for where the wrong letters will appear
 
+# Places the Frames
 Frame_top.place(x=20, y=20)
 Frame_Lmiddle1.place(x=40, y=100)
 Frame_Lmiddle2.place(x=40, y=620)
