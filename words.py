57ea5d9de972950a35c3d5e43f17b883d2929a81
RingLord54 <andrew.moneypenny@btinternet.com>
2022-05-20 20:13:51 +0100

    update

---
+++ words.py
@@ -0,0 +1,7 @@
+# words that can be chosen
+words = [
+    "hello", "there", "share", "drain", "brain", "grain", "dance",
+    "drink", "helps", "carer", "baker", "biker", "cycle", "rides",
+    "likes", "brink", "green", "melts", "prize", "print", "rinse",
+    "sinks", "blink", "makes", "chess", "queen", "quiet", "quite"
+]
